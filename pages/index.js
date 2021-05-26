6dc9b5aa3250652fcddfd4d1f884e5b861034576
Jonah Barrett <jonahb3@gmail.com>
2021-05-26 12:22:56 -0600

    Rendering posts now, something seems weird in the styling

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,14 @@
 // https://nextjs.org/docs/basic-features/pages
 import { getPosts } from "@lib/firebase";
 import styles from "@styles/index.module.scss";
-
-const getFormattedDate = (milliseconds) => {
-  const formatOptions = {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-    timeZone: "UTC",
-  };
-  const date = new Date(milliseconds);
-  return date.toLocaleDateString(undefined, formatOptions);
-};
+import { getFormattedDate } from "@lib/utils";
 
 const HomePage = ({ posts }) => (
   <div className={styles.HomePage}>
     <h1>Blog Posts</h1>
     {posts.map((post) => (
       <article key={post.slug}>
-        <a class="nostyle" href={`/post/${post.slug}`}>
+        <a href={`/post/${post.slug}`}>
           <img src={post.coverImage} alt={post.coverImageAlt} />
           <div>
             <h2>{post.title}</h2>
