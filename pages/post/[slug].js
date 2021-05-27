ab7033bcd05928e5638e562084641ef4086b3273
Jonah Barrett <jonahb3@gmail.com>
2021-05-27 10:34:28 -0600

    Posts can be deleted too now

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,11 +1,13 @@
-import { getPostBySlug } from "@lib/firebase";
+import { deletePost, getPostBySlug } from "@lib/firebase";
 import styles from "@styles/post.module.scss";
 import { getFormattedDate } from "@lib/utils";
-import { Layout } from "@components";
 import { useRouter } from "next/router";
+import { Icon, Layout } from "@components";
+import { useAuth } from "@contexts/auth";
 
 const PostPage = ({ post }) => {
   const router = useRouter();
+  const [user] = useAuth();
 
   if (!post && typeof window !== "undefined") {
     router.push("/404");
@@ -20,7 +22,30 @@ const PostPage = ({ post }) => {
     <Layout>
       <div className={styles.PostPage}>
         <img src={post.coverImage} alt={post.coverImageAlt} />
-        <h1>{post.title}</h1>
+        <div>
+          <h1>{post.title}</h1>
+          {user && (
+            <span>
+              <a href={`/edit/${post.slug}`}>
+                <Icon name="pencil-alt" />
+              </a>
+              <button
+                onClick={() => {
+                  const shouldDeletePost = confirm(
+                    "Are you sure you want to delete this post?"
+                  );
+                  if (shouldDeletePost) {
+                    deletePost(post.slug).then(() => {
+                      router.push("/");
+                    });
+                  }
+                }}
+              >
+                <Icon name="trash-alt" />
+              </button>
+            </span>
+          )}
+        </div>
         <span>Published {getFormattedDate(post.dateCreated)}</span>
         <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
       </div>
